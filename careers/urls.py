7d8d5b2fc650b1df47348daed6928a817dd24489
Your Name <you@example.com>
2021-01-15 15:14:14 +0300

    first commit

---
+++ careers/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path("", views.VacanciesView.as_view()),
+    path("vacancies/", views.VacanciesListAPIView.as_view()),
+    path("<int:pk>/", views.VacanciesDetailView.as_view()),
+    path("vacancies/<int:pk>", views.VacanciesDetailAPIView.as_view()),
+    path("request/", views.RequestCreateView.as_view()),
+    path("practice/", views.AddPracticeView.as_view()),
+    path("practice/<int:pk>", views.DeletePracticeView.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
